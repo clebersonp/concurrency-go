6de2a13b6e75074dd8d9a3c160432a6a22345b63
Cleberson <pauluci.cleberson@gmail.com>
2024-12-07 11:11:47 -0300

    Added more explanation about deadlock

--- ch1/deadlocks/main.go
+++ ch1/deadlocks/main.go
@@ -6,6 +6,9 @@ import (
 	"time"
 )
 
+// A deadlocked program is one in which all concurrent processes are waiting on one another.
+// In this state, the program will never recover without outside intervention.
+
 func main() {
 
 	type value struct {
